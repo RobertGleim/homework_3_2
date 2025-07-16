629da4f3133a69afc121123a835ff1b5d94d5449
Robert Gleim <rgleim@outlook.com>
2025-07-16 15:15:27 -0500

    homework module 3 day 2 task tracker

---
+++ Tasklist_3_2.py
@@ -0,0 +1,21 @@
+class Task:
+    def __init__ (self.name):
+        self.name = name
+        self.completed = "Incomplete"
+
+
+        
+
+class Node:
+    def __init__(self, task):
+        self.task = task
+        self.next = None)
+        
+        
+class TaskList:
+    def __init__(self):
+        self.head = None
+                
+    def.append(self,task):
+        new_node = Node(task)            
+    
